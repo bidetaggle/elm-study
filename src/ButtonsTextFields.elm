127589ddd75bbb8a1f10cdccf7ca62a1a58f7775
bidetaggle <bidetaggle@asus-UX534F.localdomain>
2019-10-30 15:12:55 +0100

    last commit clean

--- src/ButtonsTextFields.elm
+++ src/ButtonsTextFields.elm
@@ -6,9 +6,8 @@ module ButtonsTextFields exposing (..)
 --   https://guide.elm-lang.org/architecture/buttons.html
 --
 
-
 import Browser
-import Html exposing (Html, button, div, input, Attribute, text)
+import Html exposing (Attribute, Html, button, div, input, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
 
@@ -16,46 +15,66 @@ import Html.Events exposing (onClick, onInput)
 
 -- MAIN
 
+
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
+
+
 
 -- MODEL
 
+
 type alias Model =
-  { n : Int
-  , step : Int
-  }
+    { n : Int
+    , step : Int
+    }
+
 
 init : Model
 init =
-  { n = 0
-  , step = 1
-  }
+    { n = 0
+    , step = 1
+    , lol = True
+    }
+
+
 
 -- UPDATE
 
+
 type Msg
-  = Increment Int
-  | Decrement Int
-  | Reset
-  | ChangeStep String
+    = Increment Int
+    | Decrement Int
+    | Reset
+    | ChangeStep String
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Increment step -> { model | n = model.n + model.step }
-    Decrement step -> { model | n = model.n - model.step }
-    Reset -> init
-    ChangeStep newStep -> { model | step = Maybe.withDefault 0 <| String.toInt newStep }
+    case msg of
+        Increment step ->
+            { model | n = model.n + model.step }
+
+        Decrement step ->
+            { model | n = model.n - model.step }
+
+        Reset ->
+            init
+
+        ChangeStep newStep ->
+            { model | step = Maybe.withDefault 0 <| String.toInt newStep }
+
+
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
-  div []
-    [ input [ placeholder "incrementation step", value (String.fromInt model.step), onInput ChangeStep ] []
-    , button [ onClick (Decrement 2) ] [ text "-" ]
-    , div [] [ text (String.fromInt model.n) ]
-    , button [ onClick (Increment 3) ] [ text "+" ]
-    , button [ onClick Reset ] [ text "reset" ]
-    ]
+    div []
+        [ input [ placeholder "incrementation step", value (String.fromInt model.step), onInput ChangeStep ] []
+        , button [ onClick (Decrement 2) ] [ text "-" ]
+        , div [] [ text (String.fromInt model.n) ]
+        , button [ onClick (Increment 3) ] [ text "+" ]
+        , button [ onClick Reset ] [ text "reset" ]
+        ]
